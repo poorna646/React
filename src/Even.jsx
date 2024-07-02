2b25a1460d951c5cf106a230c7ada7766cbd89fe
POORNASHREE T <107635736+poorna646@users.noreply.github.com>
2024-07-02 14:44:53 +0530

    Delete src directory

--- src/Even.jsx
+++
@@ -1,9 +0,0 @@
-export default function Even({count})
-{
-    if(count%2===0)
-        // console.log(count); // this will be rendered twicw in console, so take the React.strictmode
-        return <p>number is Even</p>;   
-    else
-    return <p>number is Odd</p>;
-}
-
